ff50f94012d184a0a5d554e1a9663de227e6ff4f
Ian Huff <ianhuff@Ians-MacBook-Pro.local>
2022-03-11 13:54:31 -0800

    add helpers, no main code changes yet

--- src/cellMetadataHelpers.ts
+++ src/cellMetadataHelpers.ts
@@ -1,1 +1,16 @@
 import * as vscode from 'vscode';
+
+// Retrieve the cell run group value
+export function getCellRunGroupValue(cell: vscode.NotebookCell): string {
+    return cell.metadata.notebookRunGroups || '';
+}
+
+// Update cell metadata with the new run group value
+export function updateCellRunGroupValue(cell: vscode.NotebookCell, newGroupValue: string) {
+    const metadata = { ...(cell.metadata || {}), notebookRunGroups: newGroupValue };
+
+    // Perform our actual replace and edit
+    const wsEdit = new vscode.WorkspaceEdit();
+    wsEdit.replaceNotebookCellMetadata(cell.document.uri, cell.index, metadata);
+    vscode.workspace.applyEdit(wsEdit);
+}
